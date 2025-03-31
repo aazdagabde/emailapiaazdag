9020e43b88e020828c1ec287d69186ae767dd296
aazdag <abdellah.aazdag.23@ump.ac.ma>
2025-03-31 17:59:58 +0000

    v

---
+++ demo/src/main/java/com/aazdag/pf/controller/ContactController.java
@@ -0,0 +1,36 @@
+package com.aazdag.pf.controller;
+
+
+import com.aazdag.pf.dto.ContactForm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/contact")
+@CrossOrigin(origins = "http://localhost:4200")
+public class ContactController {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @PostMapping("/send")
+    public String sendEmail(@RequestBody ContactForm form) {
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        
+        message.setFrom(form.getEmail());
+        message.setTo("aazdag.abdellah@gmail.com"); // ton email personnel
+        message.setSubject(form.getSubject());
+        message.setText(
+            "Nom : " + form.getName() + "\n" +
+            "E-mail : " + form.getEmail() + "\n\n" +
+            form.getMessage()
+        );
+
+        mailSender.send(message);
+
+        return "Email envoyé avec succès";
+    }
+}
---
+++ demo/src/test/java/com/aazdag/pf/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aazdag.pf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
